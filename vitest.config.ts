31b0aeee41f1a1bdba20feb98491046df8848ea9
aeironnsarmiento <aeironnsarmiento@gmail.com>
2026-07-26 15:55:23 -0500

    file reorg

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["./vitest.setup.ts"],
+    setupFiles: ["./test/setup.ts"],
     include: ["**/*.test.{ts,tsx}"],
     exclude: ["node_modules/**", ".next/**"],
   },
